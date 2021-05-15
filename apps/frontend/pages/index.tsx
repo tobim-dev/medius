0062c6ff1ab3a2103bcc18baaddaa241eb4f0ccd
tobim-dev <mail@tmeyer.me>
2021-05-15 12:01:17 +0200

    configure frontend

---
+++ apps/frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div>
+      <p className="bg-gray-100"> Ein toller Paragraf</p>
+    </div>
+  );
+}
+
+export default Index;
